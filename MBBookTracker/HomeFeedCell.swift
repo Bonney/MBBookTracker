5200435730dc04e085e5f19e5ac36f178736adf1
Matt Bonney <bonneym@gmail.com>
2022-11-09 20:43:30 -0500

    Light redesign of cell layout.

--- MBBookTracker/HomeFeedCell.swift
+++ MBBookTracker/HomeFeedCell.swift
@@ -11,11 +11,13 @@ struct HomeFeedCell: View {
     var color: Color = Color.init(hue: Double.random(in: 0...360)/360, saturation: 1.0, brightness: 1.0)
     var showHeader: Bool
     var showAddButton: Bool
+    var showStars: Bool
     var showReview: Bool
 
-    init(showHeader: Bool = true, showAddButton: Bool = true, showReview: Bool = true) {
+    init(showHeader: Bool = true, showAddButton: Bool = true, showStars: Bool = true, showReview: Bool = true) {
         self.showHeader = showHeader
         self.showAddButton = showAddButton
+        self.showStars = showStars
         self.showReview = showReview
     }
 
@@ -28,35 +30,21 @@ struct HomeFeedCell: View {
 
             bookDetails()
 
-            if showAddButton {
-                addToLibraryButton()
+
+            if (showReview || showStars) {
+                Divider().padding(.vertical, 4)
+            }
+
+            if showStars {
+                starRating()
             }
 
             if showReview {
-                Divider()
-                    .padding(.vertical, 4)
+                writtenReview()
+            }
 
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum, mi sit amet porta cursus, tortor mauris volutpat libero, ut laoreet mi diam eu sem. Integer volutpat in nisl id ornare. Fusce vulputate lorem lectus, a ultrices libero feugiat at. Curabitur tincidunt congue enim, mollis finibus neque faucibus et. Vivamus eget lacus id est tincidunt bibendum at sit amet eros. Suspendisse eget vulputate ante, non fringilla urna. Fusce vel iaculis tortor, non imperdiet justo. Donec ultricies ante a nulla luctus, laoreet ultricies mi venenatis. In rutrum pulvinar eros, interdum ultrices leo porta eu. Aenean ultrices id purus laoreet eleifend. Ut malesuada nunc ligula, ut posuere diam fringilla quis.")
-                    .lineLimit(3)
-                    .font(.subheadline)
-
-                HStack {
-                    Button { } label: {
-                        Label("3 Likes", systemImage: "hand.thumbsup")
-                            .foregroundStyle(.primary)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                    }
-
-                    Button { } label: {
-                        Label("Comment", systemImage: "bubble.right")
-                            .foregroundStyle(.primary)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                    }
-                }
-                .symbolVariant(.fill)
-                .buttonStyle(.bordered)
-                .controlSize(.small)
-                .padding(.vertical, 4)
+            if showAddButton {
+                addToLibraryButton()
             }
         }
     }
@@ -88,8 +76,7 @@ struct HomeFeedCell: View {
     var bookWidth: Double { bookHeight * 2/3 }
 
     func bookDetails() -> some View {
-        HStack(alignment: .top) {
-
+        HStack(alignment: .center) {
             RoundedRectangle(cornerRadius: 4, style: .continuous)
                 .frame(width: bookWidth, height: bookHeight)
                 .foregroundStyle(color)
@@ -101,43 +88,25 @@ struct HomeFeedCell: View {
                         .foregroundStyle(.regularMaterial)
                 }
 
-            VStack(alignment: .leading, spacing: 8) {
+            VStack(alignment: .leading, spacing: 4) {
+                Text("The 7½ Deaths of Evelyn Hardcastle")
+                    .font(.system(.title3, design: .serif))
+                    .fontWeight(.medium)
+                    .foregroundStyle(.primary)
+                    .fixedSize(horizontal: false, vertical: true)
 
-                bookTitle()
-
-                HStack(spacing: 1) {
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star")
-                }
-                .foregroundStyle(.orange)
-                .imageScale(.small)
-
-                Text("Read from 07/15/22 – 11/07/22")
-                    .font(.caption)
+                Text("by Stuart Turton")
+                    .font(.subheadline)
                     .foregroundStyle(.secondary)
 
+                Text("Mystery")
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    .textCase(.uppercase)
+                    .foregroundStyle(.tint)
             }
-
         }
     }
-
-    func bookTitle() -> some View {
-        VStack(alignment: .leading) {
-            Text("The 7½ Deaths of Evelyn Hardcastle")
-                .font(.system(.body, design: .serif))
-                .fontWeight(.medium)
-                .foregroundStyle(.primary)
-                .fixedSize(horizontal: false, vertical: true)
-
-            Text("by Stuart Turton")
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-        }
-    }
-
     func addToLibraryButton() -> some View {
         Menu {
             Section {
@@ -154,6 +123,54 @@ struct HomeFeedCell: View {
         .menuStyle(.button)
         .buttonStyle(.borderedProminent)
         .controlSize(.regular)
+    }
+
+    func starRating() -> some View {
+        VStack(alignment: .leading, spacing: 2) {
+            LabeledContent("Matt's Review") {
+                HStack(spacing: 1) {
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star")
+                }
+                .foregroundStyle(.orange)
+                .imageScale(.small)
+            }
+            .font(.subheadline)
+            .fontWeight(.medium)
+
+            Text("Read from 07/15/22 to 11/07/22")
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+        }
+    }
+
+    @ViewBuilder func writtenReview() -> some View {
+        GroupBox {
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum, mi sit amet porta cursus, tortor mauris volutpat libero, ut laoreet mi diam eu sem. Integer volutpat in nisl id ornare. Fusce vulputate lorem lectus, a ultrices libero feugiat at. Curabitur tincidunt congue enim, mollis finibus neque faucibus et. Vivamus eget lacus id est tincidunt bibendum at sit amet eros. Suspendisse eget vulputate ante, non fringilla urna. Fusce vel iaculis tortor, non imperdiet justo. Donec ultricies ante a nulla luctus, laoreet ultricies mi venenatis. In rutrum pulvinar eros, interdum ultrices leo porta eu. Aenean ultrices id purus laoreet eleifend. Ut malesuada nunc ligula, ut posuere diam fringilla quis.")
+                .lineLimit(4)
+                .font(.subheadline)
+        }
+
+        HStack {
+            Button { } label: {
+                Label("3 Likes", systemImage: "hand.thumbsup")
+                    .foregroundStyle(.primary)
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+
+            Button { } label: {
+                Label("Comment", systemImage: "bubble.right")
+                    .foregroundStyle(.primary)
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+        }
+        .symbolVariant(.fill)
+        .buttonStyle(.bordered)
+        .controlSize(.regular)
+        .padding(.vertical, 4)
     }
 
 
@@ -177,6 +194,12 @@ struct HomeFeedCell_Previews: PreviewProvider {
                 }
                 Section {
                     HomeFeedCell(showHeader: false, showAddButton: false, showReview: false)
+                }
+                Section {
+                    HomeFeedCell(showHeader: true, showAddButton: true, showStars: false, showReview: false)
+                }
+                Section {
+                    HomeFeedCell(showHeader: true, showAddButton: true, showStars: true, showReview: false)
                 }
             }
         }
